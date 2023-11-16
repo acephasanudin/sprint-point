c5c1f6a539fea33ab5c72fc55c1e43b3fa8d8890
Acep Hasanudin <acep@genflix.co.id>
2023-11-16 07:49:28 +0700

    feat: modify middleware for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-    const session = request.cookies.get('next-auth.session-token')
+    const key = process.env.NEXTAUTH_URL?.startsWith("https://")
+      ? "__Secure-next-auth.session-token"
+      : "next-auth.session-token";
+    const session = request.cookies.get(key)
     if (session) return NextResponse.next();
     return NextResponse.redirect(new URL('/', request.url))
 }
