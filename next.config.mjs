6cb09f6a732f005353ae2c7ba7b586f78bd2f320
acephasanudin <acephasanudin24@gmail.com>
2023-12-06 14:18:32 +0700

    feat: add swcMinify: false in config for charts

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,17 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    reactStrictMode: true,
+    swcMinify: false,
+    /**
+     * If you are using `appDir` then you must comment the below `i18n` config out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
 };
 
 export default config;
