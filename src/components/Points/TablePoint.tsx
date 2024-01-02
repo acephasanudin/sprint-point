b0c7e48b1ec57a22a0b2d45388c8e36b35532254
acephasanudin <acephasanudin24@gmail.com>
2024-01-02 11:21:16 +0700

    feat: add delete button on filled item point

--- src/components/Points/TablePoint.tsx
+++ src/components/Points/TablePoint.tsx
@@ -141,7 +141,7 @@ export function TablePoint({ taskId, type, point, btnAdd }: any) {
                 />
             </td>
             <td>
-                <form onSubmit={(e) => {
+                <form className="float-right pr-1 pl-1" onSubmit={(e) => {
                     e.preventDefault();
                     if (!btnAdd) {
                         deleteMutation(point?.id);
@@ -160,6 +160,20 @@ export function TablePoint({ taskId, type, point, btnAdd }: any) {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                         </button>}
+                </form>
+                <form className="float-right pr-1 pl-1" onSubmit={(e) => {
+                    e.preventDefault();
+                    if (active) {
+                        deleteMutation(point?.id);
+                    }
+                }}>
+                    {active && btnAdd ?
+                        <button className="btn btn-circle btn-outline btn-error">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button> : ''
+                    }
                 </form>
             </td>
         </tr>
