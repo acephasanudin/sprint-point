0b76170091e0cf8bb197ca3cd905ff80df913c48
acephasanudin <acephasanudin24@gmail.com>
2024-01-29 20:07:18 +0700

    feat: add page

--- src/components/Tasks/CardTask.tsx
+++ src/components/Tasks/CardTask.tsx
@@ -34,6 +34,9 @@ export function CardTask() {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
                         </svg>
                     </button>
+                    <div className="join">
+                        <input className="join-item btn btn-square" type="radio" name="options" aria-label={`${iTask + 1} / ${taskIds?.length}`} checked />
+                    </div>
                     <button className="btn btn-circle btn-primary" onClick={() => nextPrevPage(1)}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
